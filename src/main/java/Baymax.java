ba0f07da3e3f175d290ac05dd5acee5b659079c1
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-02-17 01:51:13 +0800

    Implemented Echo

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,11 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Baymax {
     public static void main(String[] args) {
-        String logo = " ____________________________________________________________\n" +
+        String  openingMessage = " ____________________________________________________________\n" +
                 " Hello! I'm Baymax\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println(logo);
+        System.out.println(openingMessage);
+
+        String horizontalLine = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String currInput = sc.nextLine();
+            if (Objects.equals(currInput, "bye")) {
+                break;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println(currInput);
+            System.out.println(horizontalLine);
+        }
+
+        String closingMessage = """
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(closingMessage);
     }
 }
