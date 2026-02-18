0021ab66173f55d6cc1065647c8a5e779c259314
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-02-18 19:00:59 +0800

    Add, List functions included

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Baymax {
+
+    public static ArrayList<String> inputList = new ArrayList<>();
 
     public static void readInput() {
         Scanner sc = new Scanner(System.in);
@@ -10,12 +13,18 @@ public class Baymax {
             String currInput = sc.nextLine();
             if (Objects.equals(currInput, "bye")) {
                 break;
+            } else if (Objects.equals(currInput, "list")) {
+                Print.printInput();
+            } else {
+                //Stores Input in Array and sends back Message
+                inputList.add(currInput);
+                System.out.println(Print.echo(currInput));
             }
-
-            //Echoes the read Input
-            System.out.println(Print.echo(currInput));
         }
     }
+
+
+
 
     public static void main(String[] args) {
 
--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -14,14 +14,26 @@ public class Print {
                 """;
     }
 
+    //Echoes the Input message
     public static String echo(String message) {
-        return Print.horizontalLine + message + "\n" +Print.horizontalLine + "\n";
+        return Print.horizontalLine + "added : " + message + "\n" +Print.horizontalLine + "\n";
+    }
+
+    //Prints the Input Array
+    public static void printInput() {
+        int index = 1;
+        System.out.print(Print.horizontalLine);
+        for (String currMessage : Baymax.inputList) {
+            System.out.println(index + ". " + currMessage);
+            index++;
+        }
+        System.out.print(Print.horizontalLine + "\n");
     }
 
     public static String closingMessage() {
         return """
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
+                Bye. Hope to see you soon again!
                 I must recharge now.
                 ____________________________________________________________
                 """;
