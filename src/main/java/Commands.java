7c9ffdaed3d620ebec8a60873548e3ed63b07d04
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-02-20 04:43:05 +0800

    Added new method deleteTask

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -2,15 +2,23 @@
 
 public class Commands {
 
+    //Marks Task as Done
     public static void markTask(int num) {
         Task currTask = Baymax.inputList.get(num - 1);
         currTask.isDone = true;
         Ui.printMarked(currTask.description);
     }
 
+    //Unmarks a task previously marked as done
     public static void unmarkTask(int num) {
         Task currTask = Baymax.inputList.get(num - 1);
         Baymax.inputList.get(num - 1).isDone = false;
         Ui.printUnmarked(currTask.description);
+    }
+
+    //Deletes a task based on number given
+    public static void deleteTask(int num) {
+        int index = num - 1;
+        Baymax.inputList.remove(index);
     }
 }
