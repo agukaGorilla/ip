abb714126315a1d100e8ecc1e0eab2e141f07f10
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-02-26 22:31:51 +0800

    Add JavaDoc comments to task package

--- src/main/java/baymax/task/Deadline.java
+++ src/main/java/baymax/task/Deadline.java
@@ -1,30 +1,48 @@
 package baymax.task;
 
 import baymax.BaymaxException;
-import baymax.ui.Ui;
 
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents 'Deadline' type of task.
+ * Contains description of the task and a deadline date/time to be finished by
+ */
 public class Deadline extends Task{
 
     private LocalDateTime dateTime;
-
-    // Constructor used when read from list file
+    
+    /**
+     * Constructs a Deadline task based on task description, completion status, deadline date/time
+     *
+     * @param description The description of the task
+     * @param isDone True if completed, false otherwise
+     * @param dateTime LocalDateTime variable by which the task is to be completed
+     */
     public Deadline(String description, boolean isDone, LocalDateTime dateTime) {
         super(description, isDone);
         this.dateTime = dateTime;
     }
-
-    // Constructor used while input entered by user
-    public Deadline(String description, LocalDateTime dateTime) throws BaymaxException {
+    
+    /**
+     * Constructor used to create a Deadline task based on task description and deadline date/time.
+     * Marked as incomplete by default.
+     * This is used when new Deadline tasks are entered by user.
+     *
+     * @param description The description of the task.
+     * @param dateTime LocalDateTime variable by which the task is to be completed.
+     */
+    public Deadline(String description, LocalDateTime dateTime){
         super(description);
         this.dateTime = dateTime;
         
     }
-
-    // Returns the date as a String
+    
+    /**
+     * Returns the deadline dat/time by which the task is to be completed.
+     *
+     * @return LocalDateTime variable deadline of the task.
+     */
     public LocalDateTime getDateTime() {
         return this.dateTime;
     }
