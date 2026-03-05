5941201a0060ff9ab1f1cd25a91b85ed60eb7d37
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-03-05 19:53:02 +0800

    Refactor Main.java

--- src/main/java/baymax/Main.java
+++ src/main/java/baymax/Main.java
@@ -10,20 +10,27 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * A GUI for Duke using FXML.
+ * A GUI for Baymax using FXML.
  */
 public class Main extends Application {
     
+    private static final String FXML_PATH = "/view/MainWindow.fxml";
+    
     private Baymax baymax = new Baymax();
     
+    /**
+     * Starts the JavaFX application and sets up the primary stage.
+     *
+     * @param stage The primary stage for this application.
+     */
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(FXML_PATH));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setBaymax(baymax);  // inject the baymax instance
+            fxmlLoader.<MainWindow>getController().setBaymax(baymax);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
