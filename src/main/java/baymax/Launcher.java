52ff2bf7310acf0a68bfe5d69b55de92e257be70
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-03-05 18:56:18 +0800

    Add assertions to Baymax and Launcher

--- src/main/java/baymax/Launcher.java
+++ src/main/java/baymax/Launcher.java
@@ -2,8 +2,21 @@ package baymax;
 
 import javafx.application.Application;
 
+/**
+ * A launcher class to workaround classpath issues.
+ * Acts as the primary entry point for starting the JavaFX application.
+ */
 public class Launcher {
+    
+    /**
+     * The main method that launches the JavaFX application.
+     *
+     * @param args Command line arguments passed to the application.
+     */
     public static void main(String[] args) {
+        // ASSERTION (Precondition): The arguments array provided by the system should not be null
+        assert args != null : "Command line arguments array should not be null";
+        
         Application.launch(Main.class, args);
     }
 }
