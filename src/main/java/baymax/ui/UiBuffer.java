ca0527b73624ac97f2bbcb049a2fda51465f4103
Vaibhava Ram <vaibhavaram137@gmail.com>
2026-03-05 14:54:30 +0800

    Edit and add javadoc comments

--- src/main/java/baymax/ui/UiBuffer.java
+++ src/main/java/baymax/ui/UiBuffer.java
@@ -1,6 +1,24 @@
 package baymax.ui;
 
+/**
+ * Acts as a temporary bucket to hold text output before sending it to the GUI.
+ */
 public class UiBuffer {
-    
     private static StringBuilder response = new StringBuilder();
+    
+    /**
+     * Adds text to the bucket (and prints to console just in case).
+     */
+    public static void append(String text) {
+        response.append(text).append("\n");
+    }
+    
+    /**
+     * Empties the bucket and returns all the text inside it.
+     */
+    public static String getAndClear() {
+        String finalResponse = response.toString();
+        response.setLength(0); // Empties the bucket for the next command
+        return finalResponse;
+    }
 }
